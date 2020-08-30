775f2eff5d91bc950a497a8ee99565e725ed6a62
RoksolianaMatseliukh <66692819+RoksolianaMatseliukh@users.noreply.github.com>
2020-08-31 02:45:27 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,7 @@ import ListStart from "@/components/studying/ListStart";
 import Program from "@/components/studying/Program";
 import Learned from "@/components/studying/Learned";
 import Calculator from "@/components/studying/Calculator";
-<<<<<<< HEAD
 import General from "@/components/studying/todo_done_lists/General";
-=======
->>>>>>> b5a4f95ce33accc59aff723975cc1f46d0701262
 
 export const routes = [
     {path: '', component: Home, name: 'Home'},       
@@ -18,14 +15,13 @@ export const routes = [
         {path: '', component: ListStart, name: 'ListStart'},
         {path: '/MyStudying/program', component: Program, name: 'Program'},
         {path: '/MyStudying/learned', component: Learned, name: 'Learned'},
-<<<<<<< HEAD
         {path: '/MyStudying/calculator', component: Calculator, name: 'Calculator'},
         {path: '/MyStudying/todo_done_lists', component: General, name: 'TodoDoneLists'}
-=======
-        {path: '/MyStudying/calculator', component: Calculator, name: 'Calculator'}
->>>>>>> b5a4f95ce33accc59aff723975cc1f46d0701262
     ]},
     {path: '*', component: UnknownPage, name: 'UnknownPage'}
 ]
+
+
+
 
 
